9acd1c32dd0c38ce500007c06e427187b636ddae
Carolina Marcos Cárdaba <carolinamc@camarcos.local>
2025-01-30 23:36:23 +0100

    cuatro

--- Level1/max.c
+++ Level1/max.c
@@ -1,1 +1,15 @@
-------------
+int max(int *tab, unsigned int len)
+{
+    unsigned int i = 0;
+    int res;
+    if(len == 0)
+        return(0);
+    res = tab[0];
+    while(i < len)
+    {
+        if(tab[i] > res)
+            res = tab[i];
+        i++;
+    }
+    return(res); 
+}
